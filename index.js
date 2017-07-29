c5fc81ebceaa49f3c86fce4d0dc42ed62f9e31f6
Rasmus <rasmus.talioja@gmail.com>
2017-07-30 00:25:38 +0300

    Added sidebar

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function menuFun() {
-    var x = document.getElementById('menu');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
+    var x = document.getElementById("menu").style.width;
+    if (x === "0px") {
+        document.getElementById("menu").style.width = "250px";
+        document.getElementById("mainwrapper").style.marginLeft = "250px";
+        //document.getElementsByClassName("header").style.left = "250px";
     } else {
-        x.style.display = 'none';
+        document.getElementById("menu").style.width = "0px";
+        document.getElementById("mainwrapper").style.marginLeft = "0px";
+        //document.getElementsByClassName("header").style.left = "0px";
     }
 }
