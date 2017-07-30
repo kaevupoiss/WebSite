676541c57b2360e4182818180023de060fa999ee
Rasmus <rasmus.talioja@gmail.com>
2017-07-30 15:13:02 +0300

    Deleted some commented lines.

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ function menuFun() {
     if (x === "0px") {
         document.getElementById("menu").style.width = "250px";
         document.getElementById("mainwrapper").style.marginLeft = "250px";
-        //document.getElementsByClassName("header").style.left = "250px";
     } else {
         document.getElementById("menu").style.width = "0px";
         document.getElementById("mainwrapper").style.marginLeft = "0px";
-        //document.getElementsByClassName("header").style.left = "0px";
     }
 }
